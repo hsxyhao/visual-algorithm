dc632a2037ea8429db686a3538ffcc920dc017ab
windmilkytea <806649821@qq.com>
2018-01-10 15:52:59 +0800

    完成快速排序三路排序模式可视化

--- js/sort/quick.js
+++ js/sort/quick.js
@@ -5,7 +5,7 @@ function QuickSort(config) {
 	// 排序最坏情况配置:如果true则表示随机获取基准，否则的话选定数组右边界为基准
 	this.worst = true;
 
-	this.sortType = 'two';
+	this.sortType = 'three';
 }
 
 QuickSort.prototype.sort = function(){
@@ -17,13 +17,22 @@ QuickSort.prototype.sort = function(){
 	if (!this.sortType || this.sortType === 'default' || this.sortType === 'one') {
 		this._partition = this.oneWay;
 		this._paint = this.oneDraw;
+		this.quickSort = this._quickSort;
 	} else if(this.sortType === 'two'){
 		len--;
 		this._partition = this.twoWays;
 		this._paint = this.twoDraw;
+		this.quickSort = this._quickSort;
+	} else if (this.sortType === 'three') {
+		len--;
+		this._partition = this.threeWays;
+		this._paint = this.threeDraw;
+		this.quickSort = this._quickSortThreeWays;
 	}
 	this.highlight([-1,-1,-1,-1,-1]);
-	this._quickSort(arr,0,len);
+	console.log(arr);
+	this.quickSort(arr,0,len);
+	console.log(arr);
 	this.highlight([-1,-1,-1,-1,-1]);
 	this.render(renderArr);
 }
@@ -96,6 +105,43 @@ QuickSort.prototype.twoDraw = function(step,arr){
 	ctx.fill();
 }
 
+QuickSort.prototype.threeDraw = function(step,arr){
+	let ctx = this.ctx;
+	ctx.clearRect(0,0,this.width,this.height);
+	ctx.fillStyle = '#999999';
+	let w = this.lineWidth,
+	a = step.indexes[0],
+	b = step.indexes[1],
+	l = step.indexes[2],
+	r = step.indexes[3],
+	c = step.indexes[4],
+	d = step.indexes[5];
+	for (let i = 0; i < arr.length; i++) {
+		if (i > l && i < r){
+			ctx.fillStyle = '#52995A';
+		} else {
+			ctx.fillStyle = '#999999';
+		}
+		if (i === l) {
+			// 基准
+			ctx.fillStyle = '#445D95';
+		}else if (i > l && i <= c) {
+			// 排序游标
+			ctx.fillStyle = '#139CE6';
+		}
+		if (i >= d && i <= r) {
+			// 排序游标
+			ctx.fillStyle = '#139CE6';
+		} 
+		if (i === r) {
+			// 数组右边界
+			ctx.fillStyle = '#F5653B';
+		}
+		ctx.fillRect(i*w+1, this.height , w-1, -arr[i]);
+	}
+	ctx.fill();
+}
+
 QuickSort.prototype.draw = function(step,arr){
 	this._paint(step,arr);
 }
@@ -113,6 +159,33 @@ QuickSort.prototype._quickSort = function(arr,l,r){
 	let p = this._partition(arr,l,r);
 	this._quickSort(arr,l,p-1);
 	this._quickSort(arr,p+1,r);
+}
+
+QuickSort.prototype._quickSortThreeWays = function(arr,l,r){
+	if (l >= r) {
+		return;
+	}
+	let v = arr[l],
+	lt = l,
+	gt = r + 1,
+	i = l + 1;
+	while(i < gt){
+		if (arr[i] < v) {
+			this.quick(arr,[i,lt+1,l,r,i,gt]);
+			lt++;
+			i++;
+		} else if (arr[i] > v) {
+			this.quick(arr,[i,gt-1,l,r,i,gt]);
+			gt--;
+		} else {
+			i++
+			this.highlight([i,gt,l,r,i,gt]);
+		}
+	}
+	this.quick(arr,[l,lt,l,r,i,gt]);
+
+	this._quickSortThreeWays(arr,l,lt-1);
+	this._quickSortThreeWays(arr,gt,r);
 }
 
 QuickSort.prototype.twoWays = function(arr,l,r){
